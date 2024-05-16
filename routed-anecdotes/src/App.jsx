016c440db28fd792f10a823d00b12703f208d881
Aleksi Ojansivu <aleksi.ojansivu@gmail.com>
2024-05-16 22:13:13 +0300

    ex7.5

--- routed-anecdotes/src/App.jsx
+++ routed-anecdotes/src/App.jsx
@@ -79,6 +79,12 @@ const CreateNew = (props) => {
     navigate("/")
   }
 
+  const resetFields = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -97,6 +103,7 @@ const CreateNew = (props) => {
         </div>
         <button>create</button>
       </form>
+      <button onClick={resetFields}>reset</button>
     </div>
   )
 
